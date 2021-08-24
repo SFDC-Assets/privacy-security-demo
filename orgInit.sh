339e992d1a87b507e00c878e5887905d6657e9e7
chadevanssf <chad.evans@salesforce.com>
2021-08-24 14:33:07 -0400

    Privacy Center should not prompt user
    Added noprompt to package installation due to Heroku permission ask for remote sites.

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix privacy -o center.demo
 
 # Install Privacy Center
-sfdx force:package:install --package=04t3t000002zQ6IAAU --wait 15 
+sfdx force:package:install --package=04t3t000002zQ6IAAU --wait 15 --noprompt
 
 sfdx shane:user:psl -n 'Privacy Center User' -g User -l User
 sfdx force:user:permset:assign -n PrivacyCenter
