d66fdd475bce01907450a62507eb21d8f9a62bf5
zhou059 <zhou059@gmail.com>
2024-10-22 09:34:19 -0700

    stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix privacy -o center.demo
 
-# Install Privacy Center
-sfdx force:package:install --package=04t3t000002lfh2 --wait 15 --noprompt
+# Install Privacy Center https://sfdc.co/privacycenter-install
+sfdx force:package:install --package=04t3t000001t3LQ --wait 15 -r
 
 sfdx shane:user:psl -n 'Privacy Center User' -g User -l User
 sfdx force:user:permset:assign -n PrivacyCenter
