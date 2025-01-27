cd0faf5eb51ba88da8dad18c2fc1c1c2c8a4843d
zhou059 <zhou059@gmail.com>
2025-01-27 15:10:21 -0800

    no package

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p privacy -e center.demo
 
 # Install Privacy Center
-sf package install --package=04t3t000002lfh2 --wait 15 --noprompt
+# sf package install --package=04t3t000002lfh2 --wait 15 --noprompt
 
 sf shane user psl -n 'Privacy Center User' -g User -l User
 sf org assign permset -n PrivacyCenter
@@ -10,6 +10,6 @@ sf project deploy start
 
 sf demoutil user password set -p salesforce1 -g User -l User
 
-sf automig load --inputdir ./cmdt
+# sf automig load --inputdir ./cmdt
 
 sf org open
