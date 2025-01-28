484d89f4137e12ca0504ab1907d992184d3545f4
zhou059 <zhou059@gmail.com>
2025-01-27 16:24:23 -0800

    no package

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@ sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p pri
 # sf package install --package=04t3t000002lfh2 --wait 15 --noprompt
 
 sf shane user psl -n 'Privacy Center User' -g User -l User
-sf org assign permset -n PrivacyCenter
+# sf org assign permset -n PrivacyCenter
 
 sf project deploy start 
 
